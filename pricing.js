22f8b7b68eb7c97d90526b0a1295031893020844
GmailHelene <65661117+GmailHelene@users.noreply.github.com>
2025-07-17 19:40:09 +0000

    sfsfs

---
+++ pricing.js
@@ -0,0 +1,5 @@
+const pricingInfo = {
+    basic: { price: '99', description: 'Basic Plan' },
+    pro: { price: '199', description: 'Pro Plan' },
+    premium: { price: '299', description: 'Premium Plan' }
+};
